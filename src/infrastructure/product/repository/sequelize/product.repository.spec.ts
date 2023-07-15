9e1a463b0b45f18221efccf6893d1288a528b753
Italo Gabriel <Italog03@gmail.com>
2023-07-15 15:35:21 -0300

    refactor add infrastructure modules

--- src/infrastructure/product/repository/sequelize/product.repository.spec.ts
+++ src/infrastructure/product/repository/sequelize/product.repository.spec.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
-import ProductModel from "../db/sequelize/model/product.model";
+import ProductModel from "./product.model";
 
 import ProductRepository from "./product.repository";
-import Product from "../../domain/product/entity/product";
+import Product from "../../../../domain/product/entity/product";
 
 describe("Product repository tests", () => {
   let sequelize: Sequelize;
